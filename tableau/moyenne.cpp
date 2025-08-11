d58c7453eb2357ff6ae41abd727f5c1fa44a47d5
amira-abi <3camiraadjabi@gmail.com>
2025-08-11 20:52:44 +0200

    new exo about adding numbeers to an array until the users enter a negative number

---
+++ tableau/moyenne.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::cout << "entrer des valeurs : " << std::endl;
+    std::vector<int> valeurs {};
+    int ajout {0};
+    while(ajout>=0){
+        while(std::cin>>ajout)
+
+    }
+}
