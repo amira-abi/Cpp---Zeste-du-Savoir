0ec5ebe439e918b4830d50912edb5b0ea71a09b2
amira-abi <3camiraadjabi@gmail.com>
2025-08-13 17:30:13 +0200

    calculate sum and average from user input

--- tableau/moyenne.cpp
+++ tableau/moyenne.cpp
@@ -5,8 +5,26 @@ int main(){
     std::cout << "entrer des valeurs : " << std::endl;
     std::vector<int> valeurs {};
     int ajout {0};
-    while(ajout>=0){
-        while(std::cin>>ajout)
-
+    int somme {0};
+    int moyenne {0};
+    while(true){
+        while(!(std::cin >> ajout)){
+            std::cout << " Tu dois rentrer un entier valide !" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(255, '\n');
+        }
+        if(ajout<0){
+            break;
+        }
+        valeurs.push_back(ajout);
     }
+
+    for(auto const x : valeurs){
+        somme+= x;
+    }
+    std::cout << " La somme vaut : " << somme << std::endl;
+    moyenne = somme/(std::size(valeurs));
+
+    std::cout << " la moyenne du tableau est : " << moyenne << std::endl;
+    return 0;
 }
