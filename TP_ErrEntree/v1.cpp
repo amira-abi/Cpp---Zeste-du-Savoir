7ec33e06a6af111af265ffb41d3b1b50ec70fc53
amira-abi <3camiraadjabi@gmail.com>
2025-08-13 17:47:17 +0200

    fixed some issues

--- TP_ErrEntree/v1.cpp
+++ TP_ErrEntree/v1.cpp
@@ -5,33 +5,32 @@ int main(){
     std::cout << "Quel mois es-tu né ? ";
     int mois { 0 };
     std::cin >> mois;
-    while(!(std::cin >> mois) && (jour<1 || jour>12)){
-        std::cout << "Tu n'as pas rentré un entier valide !" << std::endl;
-        std::cin.clear();
-        std::ignore(255,'\n');
-    };
+    int jour { 0 };
 
     std::cout << "Quel jour es-tu né ? ";
-    int jour { 0 };
     while(!(std::cin >> jour) || (jour<1 || jour>31)){  
-        else{
-            if(mois%2==0 && jours==31){
-                std::cout << "Il n'y a que 30 jours pendant les mois pairs (sauf fevrier)!" << std::endl;
-            }
+        if(mois%2==0 && jour==31){
+            std::cout << "Il n'y a que 30 jours pendant les mois pairs (sauf fevrier)!" << std::endl;
         }
         std::cout << "Tu n'as pas rentré un entier valide !" << std::endl;
         std::cin.clear();
-        std::ignore(255,'\n');
+        std::cin.ignore(255,'\n');
     };
-    while(!(std::cin >> jour) || mois==2 && (jour<1 || jours>28)){
+
+    while(!(std::cin >> mois) && (jour<1 || jour>12)){
+        std::cout << "Tu n'as pas rentré un entier valide !" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(255,'\n');
+    };
+    while(!(std::cin >> jour) || mois==2 && (jour<1 || jour>28)){
         std::cout << "Il n'y a QUE 28 jours en fevrier " << std::endl;
         std::cin.clear();
-        std::ignore(255,'\n');
+        std::cin.ignore(255,'\n');
     }
-    while(!(std::cin >> jour) ||  (mois%2==0 && (jour<1 || jours>31))){
+    while(!(std::cin >> jour) ||  (mois%2==0 && (jour<1 || jour>31))){
         std::cout << "Il n'y a que 30 jours pendant les mois pairs (sauf fevrier)!" << std::endl;
         std::cin.clear();
-        std::ignore(255,'\n');
+        std::cin.ignore(255,'\n');
     }
 
 
