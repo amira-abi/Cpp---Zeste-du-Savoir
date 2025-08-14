9a05f1d18e362323f7866a25c0ce281ffb50f363
amira-abi <3camiraadjabi@gmail.com>
2025-08-14 18:05:22 +0200

    this programm give the smaller and the greater values of an array of integer

---
+++ tableau/minAndMax.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    
+    std::vector<int> values {-17, 42, -5, 89, -33, 0, 14, -76};
+    int min {values[0]};
+    int max {values[0]};
+
+    for( auto const x : values){
+        if(x<min){
+            min = x;
+        }else if(x>max){
+            max = x;
+        }
+    }
+
+    std::cout << " La liste de nombre est" << std::endl;
+    for(auto const x : values){
+        std::cout << x << ", " << std::endl;
+    }
+
+    std::cout << "Le plus petit élément du tableau est : "<< min << std::endl;
+    std::cout << "Le plus grand élément du tableau est : "<< max << std::endl;
+
+}
