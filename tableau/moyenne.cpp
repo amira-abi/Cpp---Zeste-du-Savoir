7e4f6f06d25fc8d054330dc744529cc9c80b533c
amira-abi <3camiraadjabi@gmail.com>
2025-08-14 17:52:35 +0200

    Added an empty array check before calculating average, translated all console messages to English

--- tableau/moyenne.cpp
+++ tableau/moyenne.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 
 int main(){
-    std::cout << "entrer des valeurs : " << std::endl;
+    std::cout << "Input values : " << std::endl;
     std::vector<int> valeurs {};
     int ajout {0};
     int somme {0};
     int moyenne {0};
     while(true){
         while(!(std::cin >> ajout)){
-            std::cout << " Tu dois rentrer un entier valide !" << std::endl;
+            std::cout << " You have to give a valid integer !" << std::endl;
             std::cin.clear();
             std::cin.ignore(255, '\n');
         }
@@ -18,13 +18,17 @@ int main(){
         }
         valeurs.push_back(ajout);
     }
-
-    for(auto const x : valeurs){
+    if(!std::empty(valeurs)){
+        for(auto const x : valeurs){
         somme+= x;
-    }
-    std::cout << " La somme vaut : " << somme << std::endl;
-    moyenne = somme/(std::size(valeurs));
+        }
+        std::cout << " The sum worth : " << somme << std::endl;
+        moyenne = somme/(std::size(valeurs));
 
-    std::cout << " la moyenne du tableau est : " << moyenne << std::endl;
+        std::cout << " The average of the array elements is  : " << moyenne << std::endl;
+    }else{
+        std::cout << "The array is empty , I can't calculate the average." << std::endl;
+    }
+    
     return 0;
 }
