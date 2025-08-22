8cb9dd3d30f9e0468744f139b30302fb5365d451
amira-abi <3camiraadjabi@gmail.com>
2025-08-22 11:58:18 +0200

    fix: program now works as expected

--- flux/statAboutFile.cpp
+++ flux/statAboutFile.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iterator>
 
 
 /**
@@ -19,16 +20,27 @@ int main(){
     file << "I prayed that he would finish,\n";
     file << "but he just kept right on,\n";
     file << "Did you guess the song?\n";
+    file.close();
         
     int nbLign {0};
     int nbChar {0};
     int nbWord {0};
-    std::string lign { ""};
+    std::string lign { "" };
+    std::ifstream fileRead {"data.txt"};
+    
+    std::cout<< "Here's the text :  " << std::endl;
+    while (std::getline(fileRead, lign))
+    {
+        std::cout << lign << std::endl;
+    }
+    fileRead.close();
+    
+    std::ifstream fileStat {"data.txt"};
 
-    while(std::getline(file, lign)){
+    while(std::getline(fileStat, lign)){
         nbLign++;
-        for(int i =0; i<std::size(lign), i++){
-            if(lign(i).isspace()){
+        for(char c : lign){
+            if(isspace(c)){
                 nbWord++;
             }else{
                 nbChar++;
@@ -37,10 +49,11 @@ int main(){
         nbWord++;
         std::cout << "In this lign : '" << lign << "', there is " << nbWord << " words, " << nbChar << " characters."<<std::endl;
         nbWord = 0;
-        nbChar =0;
+        nbChar = 0 ;
     }
+
+    fileStat.close();
+
     
     return 0;
-
-
 }
