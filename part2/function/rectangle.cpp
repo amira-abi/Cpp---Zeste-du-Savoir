249df81da97ed3c201ebd5e67f61424b2a8e2782
amira-abi <3camiraadjabi@gmail.com>
2025-08-25 18:44:21 +0200

    Created a new folder for the next chapter

---
+++ part2/function/rectangle.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void rectangle(int height, int width){
+    int a = 0;
+    int b = 0;
+    while(a<height){
+        while(b<width){
+            std::cout << "*";
+            b++;
+        }
+        a++;
+    }    
+}
+
+int main(){
+    int height = 0;
+    int width = 0;
+
+    std::cout << "Enter the height value : "<< std::endl;
+    while(std::cin >> height || height<1){
+        std::cout << "The value is wrong, try again..."<< std::endl;
+        std::cin.clear();
+        std::cin.ignore(255,'\n');
+    }
+
+    std::cout << "Enter the width value : "<< std::endl;
+    while(std::cin >> width || width<1){
+        std::cout << "The value is wrong, try again..."<< std::endl;
+        std::cin.clear();
+        std::cin.ignore(255,'\n');
+    }
+
+    rectangle(height, width);
+    return 0;
+
+}
