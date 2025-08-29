f838c8ced7cf987ba21a59878741c6eaf47f0cd5
amira-abi <3camiraadjabi@gmail.com>
2025-08-29 18:53:50 +0200

    changed git ignore(255, 'n') for std::numeric_limits

--- TP_ErrEntree/entryErrors.cpp
+++ TP_ErrEntree/entryErrors.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <optional>
-
+#include <limits>
 
 /**
  * Displays an error message indicating that the input is invalid
@@ -9,7 +9,15 @@
 void inputIsNotCorrect(){
     std::cout << "You're input is not correct, try again !" << std::endl;
     std::cin.clear();
-    std::cin.ignore(255,'\n');
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+}
+
+bool inputHaveGoodSize(int & input){
+    return ( input >std::numeric_limits<int>::min() && input <std::numeric_limits<int>::max());
+}
+
+bool inputHaveGoodSize(double & input){
+    return (input > std::numeric_limits<double>::min() && input<std::numeric_limits<double>::max());
 }
 
 /**
@@ -23,7 +31,7 @@ void inputIsNotCorrect(){
  */
 int askUser(std::string question, int & input){
     std::cout << question << std::endl;
-    while(!(std::cin >> input)){
+    while(!(std::cin >> input || !inputHaveGoodSize(input))){
         inputIsNotCorrect();
         std::cout<< "You're suppose to give an int !"<< std::endl;
     };
@@ -42,7 +50,7 @@ int askUser(std::string question, int & input){
  */
 double askUser(std::string question, double & input){
     std::cout << question << std::endl;
-    while(!(std::cin >> input)){
+    while(!(std::cin >> input) || !inputHaveGoodSize(input)){
         inputIsNotCorrect();
         std::cout<< "You're suppose to give an int !"<< std::endl;
     };
@@ -64,6 +72,7 @@ double askUser(std::string question, double & input){
 int checkInput(std::optional<int> limitA, std::optional<int> limitB, std::string question, int & input){
     input = askUser(question, input);
     while ((limitA && input < limitA.value()) || (limitB && input > limitB.value())){
+        std::cout << "Value out of bounds" << std::endl;
         inputIsNotCorrect();
         input = askUser(question, input);
     }
@@ -83,6 +92,7 @@ int checkInput(std::optional<int> limitA, std::optional<int> limitB, std::string
 double checkInput(double limitA, std::string question, double & input){
     input = askUser(question, input);
     while (input < limitA){
+        std::cout << "Value out of bounds" << std::endl;
         inputIsNotCorrect();
         input = askUser(question, input);
     }
@@ -108,7 +118,7 @@ int main(){
     size = checkInput(0.0,"What is you're size ? (in cm)", size);
 
     std::cout << "You we're born on " << day << "/" << month << "/" << year << std::endl;
-    std::cout<< "You're size is " << size << " cm"<< std::endl;
+    std::cout<< "You're size is " << size << " m"<< std::endl;
 
     return 0;
 }
