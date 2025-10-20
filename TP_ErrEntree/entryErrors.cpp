a225ccd5394663f6df778078cdc4eee7fb092c05
amira-abi <3camiraadjabi@gmail.com>
2025-10-20 21:43:12 +0200

    added a lambda function checkInput that will managed both type of variables that are managed in this code which are double int , this function is supposed to replace checkInput()

--- TP_ErrEntree/entryErrors.cpp
+++ TP_ErrEntree/entryErrors.cpp
@@ -118,37 +118,35 @@ int main(){
     int year {0};
     double size {0.0};
 
-    auto askUser = [](const std::string & question){
-        auto input;
-        std::cout << question;
-        while(!(std::cin >> input || !inputHaveGoodSize(input))){
-        if (std::cin.eof())
-        {
-            throw std::runtime_error("Input stream has been closed!")
-        }else{
-            clearInput();
-            std::cout<< "You're suppose to give an int or a double(size) !"<< std::endl;
-        }
-        std::cout<< "You're suppose to give an int !"<< std::endl;
+    auto checkInput = [](auto & input, auto predicate) -> bool {
+        while(!(std::cin >> input || !predicat(input))){
+            if (std::cin.eof())
+            {
+                throw std::runtime_error("Input stream has been closed!")
+            }else if (std::cin.fail()){
+                clearInput();
+            }else{
+                std::cout<< "The predicate was not respected !"<< std::endl;
+            }
     }
         return input;
     }
 
-    askUser("On what month were you born ? ");
+    std::cout<<"On what month were you born ? "<< std::endl;
     month = checkInput(1, 12, month) ;
     
     if(month==2){
-        askUser("On what day were you born ? ");
+        std::cout<<"On what day were you born ? " << std::endl;
         day = checkInput(1, 28, day);
     }else{
-        askUser("On what day were you born ? ");
+        std::cout<< "On what day were you born ? " << std::endl;
         day = checkInput(1, 31, day);
     }
 
-    askUser("On what year were you born");
+    std::cout << "On what year were you born" << std::endl;
     year = checkInput(std::nullopt, 2025 , year);
 
-    askUser("What is you're size ? (in cm)");
+    std::cout << "What is you're size ? (in cm)" << std::endl;
     size = checkInput(0.0, size);
 
     std::cout << "You we're born on " << day << "/" << month << "/" << year << std::endl;
